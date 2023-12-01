dff297f342adcbd2cf91eeaf0bf00a663c0fb432
Amayoo0 <amayov4c@gmail.com>
2023-12-01 12:11:22 +0100

    3.3 Creación de BSE Test

---
+++ Tests/BseTest.cs
@@ -0,0 +1,29 @@
+using Moq;
+using NUnit.Framework;
+
+using RadioConformanceTests.Drivers;
+using RadioConformanceTests.Instruments;
+
+public class BseTests
+{
+    [Test]
+    public void configureCellTest(){
+        // Verificar que se realiza la llamada de los comandos de configuración correctamente
+        var mockScpi = new Mock<IScpiClient>();
+        var bse = new BseInstrument(mockScpi.Object);
+        bse.configureCell(-10, -10);
+        mockScpi.Verify(v => v.Command("BSE:CELL1:FREQ -10"));
+        mockScpi.Verify(v => v.Command("BSE:CELL1:POW -10"));
+    }
+
+    [Test]
+    public void verifyBSEInstrumentTest(){
+        // Verificar que al llamar a verifyBSEInstruments se devuelva true
+        var mockScpi = new Mock<IScpiClient>();
+        mockScpi.Setup(s => s.Query("BSE:*IDN?")).Returns("Keysight,BSE").Verifiable();
+        
+        var bse = new BseInstrument(mockScpi.Object);
+        Assert.IsTrue(bse.verifyBSEInstrument());
+        mockScpi.Verify();
+    }
+}

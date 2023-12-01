3dd64b307f12d6f2ef7a51bd02a07e3f1cb71576
Amayoo0 <amayov4c@gmail.com>
2023-12-01 14:24:17 +0100

    creación passVerdictTest

--- Tests/TestCase/TC_SensitivityTest.cs
+++ Tests/TestCase/TC_SensitivityTest.cs
@@ -3,6 +3,7 @@ using NUnit.Framework;
 
 using RadioConformanceTests.TC;
 using RadioConformanceTests.Instruments;
+using RadioConformanceTests.Drivers;
 
 public class TC_SensitivityTest{
     [Test]
@@ -10,12 +11,16 @@ public class TC_SensitivityTest{
         // Verifica que Verdict = pass
         var mockBse = new Mock<IBseInstrument>();
         mockBse.Setup(s => s.verifyBSEInstrument()).Returns(true);
-        //mockBse.Setup(s => s.startCell())
-        //mockBse.Setup(s => s.configureCell())
         mockBse.Setup(s => s.ueIsConnected()).Returns(true);
-        //mockBse.Setup(s => s.endCell())
+/*      DUDA: No se deberían Mockear el resto de métodos utilizados en LC_Sensitivity?¿
+        mockBse.Setup(s => s.endCell());
+        mockBse.Setup(s => s.startCell());
+        mockBse.Setup(s => s.configureCell()); */
 
-        //var test = new TC_SensitivityTest(mockBse.Object)
+        ILogger logger = new Logger();
+        var test = new TC_Sensitivity(mockBse.Object, logger);
+
+        Assert.That(test.Execute(), Is.EqualTo(TestVerdict.Pass));
 
 
     }

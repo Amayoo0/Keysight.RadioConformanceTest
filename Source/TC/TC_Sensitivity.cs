0103efb976b15707e2b2e395520835ae68c38814
Amayoo0 <amayov4c@gmail.com>
2023-12-01 14:26:04 +0100

    Hacer pública la clase TC_Sensitivity

--- Source/TC/TC_Sensitivity.cs
+++ Source/TC/TC_Sensitivity.cs
@@ -3,7 +3,7 @@ using RadioConformanceTests.Instruments;
 
 namespace RadioConformanceTests.TC;
 
-class TC_Sensitivity 
+public class TC_Sensitivity 
 {
     /// <summary>
     /// Test case configuration

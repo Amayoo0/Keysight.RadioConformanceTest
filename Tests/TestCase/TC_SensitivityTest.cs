2f2107549fb3101a1ea787eca3a762158752c46a
Amayoo0 <amayov4c@gmail.com>
2023-12-01 21:31:01 +0100

    new Test failVerdictUELoseConnectionTest

--- Tests/TestCase/TC_SensitivityTest.cs
+++ Tests/TestCase/TC_SensitivityTest.cs
@@ -4,6 +4,7 @@ using NUnit.Framework;
 using RadioConformanceTests.TC;
 using RadioConformanceTests.Instruments;
 using RadioConformanceTests.Drivers;
+using System;
 
 public class TC_SensitivityTest{
     [Test]
@@ -22,6 +23,27 @@ public class TC_SensitivityTest{
 
         Assert.That(test.Execute(), Is.EqualTo(TestVerdict.Pass));
 
+    }
+
+    [Test]
+    public void failVerdictUELoseConnectionTest(){
+        // Verificar que da fail cuando el UE pierde la conexion
+        var mockBse = new Mock<IBseInstrument>();
+        mockBse.Setup(s => s.ueIsConnected()).Returns(() => 
+        {    
+            Random rand = new Random();
+            return rand.NextDouble() >= 0.3;
+        });
+
+        mockBse.Setup(s => s.verifyBSEInstrument()).Returns(true);
+        
+        ILogger logger = new Logger();
+        var test = new TC_Sensitivity(mockBse.Object, logger);
+
+        Assert.That(test.Execute(), Is.EqualTo(TestVerdict.Fail));
+
+        
 
     }
+
 }

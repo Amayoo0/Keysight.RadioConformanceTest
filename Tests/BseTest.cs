238130fb3737c34be5bdcaa4949e157f61a8c8c3
Amayoo0 <amayov4c@gmail.com>
2023-12-01 13:00:41 +0100

    new pass test: ueIsConnectedTest

--- Tests/BseTest.cs
+++ Tests/BseTest.cs
@@ -21,9 +21,20 @@ public class BseTests
         // Verificar que al llamar a verifyBSEInstruments se devuelva true
         var mockScpi = new Mock<IScpiClient>();
         mockScpi.Setup(s => s.Query("BSE:*IDN?")).Returns("Keysight,BSE").Verifiable();
-        
+
         var bse = new BseInstrument(mockScpi.Object);
         Assert.IsTrue(bse.verifyBSEInstrument());
+        mockScpi.Verify();
+    }
+
+    [Test]
+    public void UeIsConnectedTest(){
+        //Verifica que los tests dan PASS si el UE permanece conectado para cada cambio de frecuencia y potencia
+        var mockScpi = new Mock<IScpiClient>();
+        mockScpi.Setup(s => s.Query("BSE:CELL1:UE:CONNECTED")).Returns("1").Verifiable();
+
+        var bse = new BseInstrument(mockScpi.Object);
+        Assert.IsTrue(bse.ueIsConnected());
         mockScpi.Verify();
     }
 }
